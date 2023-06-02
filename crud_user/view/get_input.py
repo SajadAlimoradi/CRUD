187ee5ddafbefc674b14256397ed5f4cc597ff7d
Sajad Alimoradi <sajad.alimoradi@outlook.com>
2023-06-03 02:00:13 +0330

    style(view): apply flake8 in view

--- crud_user/view/get_input.py
+++ crud_user/view/get_input.py
@@ -3,36 +3,18 @@ from crud_user.control.crud import Crud
 from painless.utils import banner, clear_screen
 from pyfiglet import Figlet
 from termcolor import colored
-
 from logs.log_conf import LogCrud
 
 clear_screen()
-start_message = Figlet(font='standard', width=110)
-logging = LogCrud.log_establish()
-
-# def view():
-#     """View function for interacting with the CRUD Control."""
-#     banner()
-#     print(colored(start_message.renderText('Welcome to CRUDE v0.1.0 '), 'green'))
-#     time.sleep(3)
-#     while True:
-#         clear_screen()
-#         print(colored(start_message.renderText('C R U D'), 'blue'))
-#         print(colored(start_message.renderText('------'), 'red'))
-#         action: str = input('\nWhat do you want to do? [ Create - Read - Update - Delete - Help]  \n ').lower()
-#         data_input: Crud = Crud(action)
-#         data_input.create_user()
-#         data_input.read_user()
-#         data_input.update_user()
-#         data_input.delete_user()
-#         data_input.help_user()
+start_message: Figlet = Figlet(font='standard', width=110)
+logging: LogCrud = LogCrud.log_establish()
 
 
-def view():
+def view() -> None:
     banner()
-    print(colored(start_message.renderText('Welcome to CRUDE v0.1.0 '), 'green'))
+    print(colored(start_message.renderText('Welcome to CRUDE v0.1.0 '), 'green')) # noqa
     time.sleep(3)
-    actions = {
+    actions: dict[str, str] = {
         'create': 'create_user',
         'read': 'read_user',
         'update': 'update_user',
@@ -44,11 +26,11 @@ def view():
         clear_screen()
         print(colored(start_message.renderText('C R U D'), 'blue'))
         print(colored(start_message.renderText('------'), 'red'))
-        action = input('\nWhat do you want to do? [ Create - Read - Update - Delete - Help]  \n ').lower()
-        data_input = Crud(action)
+        action: str = input('\nWhat do you want to do? [ Create - Read - Update - Delete - Help]  \n ').lower() # noqa
+        data_input: Crud = Crud(action)
 
         if action in actions:
-            method_name = actions[action]
+            method_name: str = actions[action]
             method = getattr(data_input, method_name)
             method()
         else:
